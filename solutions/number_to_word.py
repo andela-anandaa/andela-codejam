fe644beeaac7ac557bf1506181d0b078eab94cdb
Anthony Nandaa <anthony.nandaa@andela.com>
2015-08-18 14:00:20 +0300

    Some Refactor on num_to_word(1)

--- solutions/number_to_word.py
+++ solutions/number_to_word.py
@@ -1,41 +1,32 @@
 relation = {
     0: 'zero',
-    1:'one',
+    1: 'one',
     2: 'two',
-    3:'three',
+    3: 'three',
     4: 'four',
-    5:'five',
+    5: 'five',
     6: 'six',
-    7:'seven',
+    7: 'seven',
     8: 'eight',
-    9:'nine',
-    10: 'ten'
+    9: 'nine'
 };
-
 
 def num_to_word(number):
         number = str(number)
-        x = map(int,number)
-        for val in x:
-            for d in relation:
-                if val == d:
-                    print relation[d],
-					
-					
+        for val in number:
+            print relation[int(val)],
+
+
 #Alternate and re-factored solution: @nandaa
 
-def num_to_word(num):
+def num_to_word2(num):
   num = str(num)
-  words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 
+  words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five',
            'Six','Seven', 'Eight', 'Nine']
   for i in num:
-    
+
     print words[int(i)] + " ",
 
 
 #print num_to_word(488833)
 #print num_to_word(438483478)
-
-
-
-
